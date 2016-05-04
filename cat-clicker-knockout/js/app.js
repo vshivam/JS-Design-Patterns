2b00118272398ec4ccedd960c12c9e799a13db02
vshivam <shivam.bitsgoa@gmail.com>
2016-05-04 17:18:52 +0800

    separated the model

--- cat-clicker-knockout/js/app.js
+++ cat-clicker-knockout/js/app.js
@@ -1,12 +1,15 @@
-var ClickCounterViewModel = function(name, url, nicks) {
+var ClickCounterViewModel = function(cat) {
+	this.currentCat = ko.observable(cat);
+	this.incrementCounter = function() {
+		return this.currentCat().numberOfClicks(this.currentCat().numberOfClicks() + 1);
+	};
+}
+
+var Cat = function(name, url, nicks){
 	this.name = ko.observable(name);
 	this.imgSrc = ko.observable(url);
 	this.nicks = ko.observableArray(nicks);
 	this.numberOfClicks = ko.observable(0);
-
-	this.incrementCounter = function() {
-		return this.numberOfClicks(this.numberOfClicks() + 1);
-	};
 
 	this.level = ko.computed(function() {
 		if(this.numberOfClicks() < 10){
@@ -20,5 +23,7 @@ var ClickCounterViewModel = function(name, url, nicks) {
 	}, this);
 }
 
-var viewModel = new ClickCounterViewModel("Taffy", "http://exmoorpet.com/wp-content/uploads/2012/08/cat.png", ["cool cat", "super cool cat", "woot woot"]);
+var cat = new Cat("Taffy", "http://exmoorpet.com/wp-content/uploads/2012/08/cat.png", ["cool cat", "super cool cat", "woot woot"]);
+
+var viewModel = new ClickCounterViewModel(cat);
 ko.applyBindings(viewModel);
