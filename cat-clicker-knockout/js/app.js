4b194b21f59b4a98d9b11702cfdef2d58b3b9c93
vshivam <shivam.bitsgoa@gmail.com>
2016-05-05 14:19:19 +0800

    cats show up in a list

--- cat-clicker-knockout/js/app.js
+++ cat-clicker-knockout/js/app.js
@@ -1,14 +1,37 @@
+var Data = [{
+	name : "Taffy", 
+	imgSrc : "https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg", 
+	nicks : ["taffy 1", "taffy 2"]
+}, 
+{
+	name : "Puffy", 
+	imgSrc : "http://pic.1fotonin.com/data/wallpapers/63/WDF_1084734.jpg",
+	nicks : ["puffy 1", "puffy 2"]
+}];
+
 var ClickCounterViewModel = function() {
-	this.currentCat = ko.observable();
-	this.incrementCounter = function() {
+	var self = this;
+
+	self.catList = ko.observableArray([]);
+	Data.forEach(function(data){
+		self.catList.push(new Cat(data));
+	});
+
+	self.currentCat = ko.observable(this.catList()[0]);
+
+	self.incrementCounter = function() {
 		return this.numberOfClicks(this.numberOfClicks() + 1);
+	};
+
+	self.setCurrentCat = function() {
+		self.currentCat(this);
 	};
 }
 
-var Cat = function(){
-	this.name = ko.observable("Taffy");
-	this.imgSrc = ko.observable("http://exmoorpet.com/wp-content/uploads/2012/08/cat.png");
-	this.nicks = ko.observableArray(["cool cat", "super cool cat", "woot woot"]);
+var Cat = function(data){
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nicks = ko.observableArray(data.nicks);
 	this.numberOfClicks = ko.observable(0);
 
 	this.level = ko.computed(function() {
@@ -25,6 +48,3 @@ var Cat = function(){
 
 var viewModel = new ClickCounterViewModel();
 ko.applyBindings(viewModel);
-
-var cat = new Cat();
-viewModel.currentCat(cat);
